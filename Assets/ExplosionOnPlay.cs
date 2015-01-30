98d7ebf0efe112c7c4f74f172d754948a65fbf7f
Fernand <fernand.veyrier@epitech.eu>
2015-01-30 22:53:12 +0100

    EXPLOSIONS

---
+++ Assets/ExplosionOnPlay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionOnPlay : MonoBehaviour {
+	
+	public GameObject explosionPrefab;
+	public AudioClip explosionSound;
+
+	public void OnPlayButton()
+	{
+		StartCoroutine("explosions");
+		Destroy(this.gameObject, 2.4f);
+	}
+
+	IEnumerator explosions()
+	{
+		for (int i = 0; i < 80; i++)
+		{
+			GameObject go = (GameObject) Instantiate(explosionPrefab, Random.insideUnitCircle, Quaternion.identity);
+			audio.PlayOneShot(explosionSound);
+			go.transform.SetParent(this.transform);
+			yield return new WaitForSeconds(0.01f);
+		}
+	}
+
+	public void OnQuitButton()
+	{
+		Application.Quit();
+	}
+}
