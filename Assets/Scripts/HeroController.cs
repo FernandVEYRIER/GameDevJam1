5d1928416068065d33785d8fc9444f695769b6b6
Fernand <fernand.veyrier@epitech.eu>
2015-01-30 22:06:22 +0100

    Anim ok

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -3,31 +3,47 @@ using System.Collections;
 
 public class HeroController : MonoBehaviour {
 
+	public LayerMask WhatIsGround;
 	public Transform groundCheck;
-	public float Velocity = 10f;
+	public float Velocity = 300f;
+	public float JumpForce = 300f;
 
 	bool isGrounded;
 	int walkAnim;
-	int idleAnim;
+	int jumpAnim;
 	Animator anim;
 
 	void Start () 
 	{
 		anim = this.GetComponent<Animator>();
 		walkAnim = Animator.StringToHash("Velocity");
-		idleAnim = Animator.StringToHash("Idle");
+		jumpAnim = Animator.StringToHash("isGrounded");
+		isGrounded = true;
 	}
 
 	void Update () 
 	{
-		this.rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * Velocity, 0);
-
-		if (this.rigidbody2D.velocity.x != 0)
+		//face the right direction
+		if (Mathf.RoundToInt(this.rigidbody2D.velocity.x) != 0)
 		{
-			this.transform.localScale = new Vector3((this.rigidbody2D.velocity.x > 0) ? -1 : 1, 1, 1);
+			this.transform.localScale = new Vector3((Mathf.RoundToInt(this.rigidbody2D.velocity.x) > 0) ? -1 : 1, 1, 1);
 		}
 
+		//can he jump ?
+		if (isGrounded && Input.GetAxis("Vertical") > 0)
+		{
+			isGrounded = false;
+			this.rigidbody2D.AddForce(new Vector2(0, JumpForce));
+		}
+
+		if (Physics2D.OverlapCircle(groundCheck.position, 0.1f, WhatIsGround))
+		{
+			Debug.Log("ON THE GROUND");
+			isGrounded = true;
+		}
+		//sets anim and forces
+		this.rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * Velocity, this.rigidbody2D.velocity.y);
 		anim.SetFloat(walkAnim, Mathf.Abs(Input.GetAxis("Horizontal")));
-		Debug.Log(Input.GetAxis("Horizontal"));
+		anim.SetBool(jumpAnim, isGrounded);
 	}
 }
