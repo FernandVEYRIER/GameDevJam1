20bf95363363f9934c4ea89d32e3d48cd2f8ad3a
Fernand <fernand.veyrier@epitech.eu>
2015-01-31 18:18:39 +0100

    adding crates

---
+++ Assets/HealthCrateBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthCrateBehaviour : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.collider.tag == "Ground")
+		{
+			this.rigidbody2D.isKinematic = true;
+			this.collider2D.isTrigger  = true;
+			Destroy(this.transform.GetChild(0).gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+
+	}
+}
