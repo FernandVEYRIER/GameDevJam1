0db5a44729f5e6d330c138d898a920f2aa5cc4e3
Fernand <fernand.veyrier@epitech.eu>
2015-01-31 03:29:25 +0100

    adding sounds

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioClip[] audioClips;
+
+	void Start () 
+	{
+		audio.PlayOneShot(audioClips[0]);
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
